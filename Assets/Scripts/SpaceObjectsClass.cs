6b4689f3163a043e073d44afebd5486240617b3f
GalAzu <60822668+GalAzu@users.noreply.github.com>
2022-01-28 01:26:05 +0200

    SpaceObject
    * space object script

---
+++ Assets/Scripts/SpaceObjectsClass.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class SpaceObjectsClass 
+{
+    public string objectName;
+    public int objID;
+    public GameObject spaceObject;
+
+}
