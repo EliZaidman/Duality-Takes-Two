5d3714da355236b0215feaa1365e236476242323
Eli-PC <rxelik5@gmail.com>
2022-01-28 15:12:50 +0200

    SpawnManager, EnemyAI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,43 +1,105 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
+using UnityEditor;
 public class GameManager : MonoBehaviour
 {
-    #region SINGLETON PATTERN
-    public static GameManager _instance;
+    #region Singleton
+    private static GameManager _instance;
     public static GameManager Instance
     {
         get
         {
             if (_instance == null)
-            {
-                _instance = GameObject.FindObjectOfType<GameManager>();
-
-                if (_instance == null)
-                {
-                    GameObject container = new GameObject("GameManager");
-                    _instance = container.AddComponent<GameManager>();
-                }
-            }
+                Debug.Log("Game Manager not loaded properly");
 
             return _instance;
         }
     }
     #endregion
 
-    public bool hittingLight;
+    #region Serialized Fields
+    [SerializeField]
+    private SpawnerManager _spawnerManager;
 
-    public GameObject lightPlayer;
-    public GameObject shadowPlayer;
-    private void Update()
+    //[SerializeField]
+    //private TextMeshProUGUI _countDown, _levelText, _nextLevelTxt;
+
+    [SerializeField]
+    private bool _isWaveOngoing = false;
+    #endregion
+
+    #region Properties
+    public bool IsWaveOngoing { get => _isWaveOngoing; set => _isWaveOngoing = value; }
+    #endregion
+
+    #region Public Fields
+    //public GameObject rest;
+    //public ParticleSystem hitEffect;
+    //public ParticleSystem deathEffect;
+    //public ParticleSystem warningEffect;
+    public int Level, TargetsRemaining;
+    public float Timer, TimeSinceLevelStart;
+
+    public List<GameObject> targetPosition;
+    #endregion
+
+    #region Unity Callbacks
+    private void Awake()
     {
-        if (!hittingLight)
-        {
-        }
-        //else
-        //{
-        //    lightPlayer.GetComponent<Light>().enabled = true;
-        //}
+        if (_instance == null)
+            _instance = this;
+
+        else
+            Destroy(this);
     }
+
+    void Update()
+    {
+        //_levelText.text = Level.ToString("0");
+
+        if (!IsWaveOngoing)
+        {
+            Timer -= Time.deltaTime;
+            //_countDown.text = Timer.ToString("0");
+
+            if (Timer <= 0)
+                IsWaveOngoing = true;
+        }
+
+
+        TimeSinceLevelStart += Time.deltaTime;
+        if (IsWaveOngoing == false)
+            TimeSinceLevelStart = 0;
+    }
+    #endregion
+
+    #region Methods
+    //public void NextLevel()
+    //{
+    //    if (Level < 1)
+    //    {
+    //        _nextLevelTxt.text = "Start Game!";
+    //        Timer = 5;
+    //        _spawnerManager._timeBetweenSpawns = 1;
+    //        _spawnerManager._maxSpawns += Level;
+    //        _spawnerManager._currentTimeBetweenSpawns = _spawnerManager._timeBetweenSpawns;
+    //        IsWaveOngoing = false;
+    //    }
+
+    //    else
+    //    {
+    //        _nextLevelTxt.text = "Next Level!";
+    //        Level++;
+    //        Timer = 5;
+    //        _spawnerManager._timeBetweenSpawns = 1;
+    //        _spawnerManager._maxSpawns += Level;
+    //        _spawnerManager._currentTimeBetweenSpawns = _spawnerManager._timeBetweenSpawns;
+    //        IsWaveOngoing = false;
+    //    }
+    //}
+
+
+    #endregion
 }
