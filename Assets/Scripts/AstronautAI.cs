b74c8c594a58fe7bdd388abe031282eeffa5a44d
Eli-PC <rxelik5@gmail.com>
2022-01-28 17:01:01 +0200

    AstronautFIX

--- Assets/Scripts/AstronautAI.cs
+++ Assets/Scripts/AstronautAI.cs
@@ -6,13 +6,20 @@ using UnityEngine.Events;
 public class AstronautAI : MonoBehaviour
 {
     [SerializeField] private UnityEvent OnDeath;
+    [SerializeField] private UnityEvent OnWin;
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Obsticle")
         {
             OnDeath.Invoke();
-            Debug.Log("EnteredIntoInvoke");
+            Debug.Log("");
+        } 
+
+        if (collision.gameObject.tag == "Moon")
+        {
+            OnWin.Invoke();
+            Debug.Log("");
         }
     }
 }
